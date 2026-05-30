5c2df4557d1114707bcc6df13d3ac52cd0656929
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 16:07:04 +0200

    Fix header overlap with iOS status bar using safe-area-inset-top

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 10 - bump this number to trigger update
-const CACHE_VERSION = 10;
+// Version: 11 - bump this number to trigger update
+const CACHE_VERSION = 11;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
