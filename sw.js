232715cf61b7ed4d525e6cba5f1cb7708d1c3538
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 15:32:03 +0200

    Add cache version display next to refresh button

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 6 - bump this number to trigger update
-const CACHE_VERSION = 6;
+// Version: 7 - bump this number to trigger update
+const CACHE_VERSION = 7;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
