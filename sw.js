9bd32a948049390aaad59d26c85e46079b529476
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 16:51:47 +0200

    Add current point indicator in banner + highlight cards in timeline

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 17 - bump this number to trigger update
-const CACHE_VERSION = 17;
+// Version: 18 - bump this number to trigger update
+const CACHE_VERSION = 18;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
