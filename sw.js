85d8577ce7444cc33927242eaaee6e3d8fb9377c
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 17:02:36 +0200

    Darken transit connector text between tiles

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 20 - bump this number to trigger update
-const CACHE_VERSION = 20;
+// Version: 21 - bump this number to trigger update
+const CACHE_VERSION = 21;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
