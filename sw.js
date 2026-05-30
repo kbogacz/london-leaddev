a962d3bccb377427b5877f2a20850bc177a11851
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 18:06:54 +0200

    UX improvements: sticky tracker with DEMO mode, auto-scroll to current day, wet weather Tube route, Thursday luggage at Liverpool St, fixed Google Images links

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 24 - bump this number to trigger update
-const CACHE_VERSION = 24;
+// Version: 25 - bump this number to trigger update
+const CACHE_VERSION = 25;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
