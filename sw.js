6329f82084a4c46884660b61872b2839678c35a9
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 14:18:41 +0200

    Initial PWA commit - London LeadDev travel guide

---
+++ sw.js
@@ -0,0 +1,65 @@
+// Service Worker for Londyn LeadDev PWA
+const CACHE_NAME = 'londyn-leaddev-v1';
+const URLS_TO_CACHE = [
+  './',
+  './londyn-leaddev.html',
+  './leaflet.min.js',
+  './london-map.png',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install: cache all static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[SW] Caching app shell');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests (Google Maps, external APIs, map tiles)
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then(response => {
+          // Don't cache non-successful responses
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          // Clone and cache the response
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+  );
+});
