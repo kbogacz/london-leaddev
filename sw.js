94b2ae68b5d87b03056dd81e1e2863e383bd2f5d
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 16:11:32 +0200

    Enlarge tab bar touch targets: taller buttons, bigger icons, visual feedback

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 12 - bump this number to trigger update
-const CACHE_VERSION = 12;
+// Version: 13 - bump this number to trigger update
+const CACHE_VERSION = 13;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
