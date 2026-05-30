e89673f5d0e95dca7404766b2308fa4ae5cb3c2c
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 17:06:09 +0200

    Refactor tracker: show only for Sunday (sunset) and Thursday (flight)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 21 - bump this number to trigger update
-const CACHE_VERSION = 21;
+// Version: 22 - bump this number to trigger update
+const CACHE_VERSION = 22;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
