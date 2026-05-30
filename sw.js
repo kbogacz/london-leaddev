edd803168dc51141e1e320a91a3b463823f8b80f
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 20:49:40 +0200

    Add POI numbers to cards, pin links to attractions, Tuesday after party Boom Battle Bar, fix metro directions

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 29 - bump this number to trigger update
-const CACHE_VERSION = 29;
+// Version: 30 - bump this number to trigger update
+const CACHE_VERSION = 30;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
