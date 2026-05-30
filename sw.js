6452ab0d2b9209fe82772cd19f6ef2e9de1bdf21
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 14:38:13 +0200

    Standardize metro transit instructions with colored line dots and consistent format

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 2 - bump this number to trigger update
-const CACHE_VERSION = 2;
+// Version: 3 - bump this number to trigger update
+const CACHE_VERSION = 3;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
