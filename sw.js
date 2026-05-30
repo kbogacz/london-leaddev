b3ebda2ec3aa3cb3f7d8b4fc30e14929e61157c9
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-30 14:40:45 +0200

    Fix transit route readability + move refresh button below online indicator

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 3 - bump this number to trigger update
-const CACHE_VERSION = 3;
+// Version: 5 - bump this number to trigger update
+const CACHE_VERSION = 5;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
