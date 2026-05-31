ead87cc74f065b949246972bc39b30bb9c0fecc6
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-31 11:17:18 +0200

    Add 35-min audio guide about London food culture + 19 iconic food spots in Apple Maps export

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 37 - bump this number to trigger update
-const CACHE_VERSION = 37;
+// Version: 38 - bump this number to trigger update
+const CACHE_VERSION = 38;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
