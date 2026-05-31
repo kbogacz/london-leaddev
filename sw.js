abf488babda8ada51c59808fb3637126bfe5527c
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-31 08:25:02 +0200

    Add Wicked audio guide, food items now use GM/AM modal picker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 33 - bump this number to trigger update
-const CACHE_VERSION = 33;
+// Version: 34 - bump this number to trigger update
+const CACHE_VERSION = 34;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
