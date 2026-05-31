2c6b02bb2b57aa98eb3a7a86084d0dbb56a9920c
Krzysztof Bogacz <kbogacz@gmail.com>
2026-05-31 10:25:24 +0200

    Add Apple Maps export section in Info tab - all POIs, bike docks, top restaurants with direct AM links

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Londyn LeadDev PWA
-// Version: 36 - bump this number to trigger update
-const CACHE_VERSION = 36;
+// Version: 37 - bump this number to trigger update
+const CACHE_VERSION = 37;
 const CACHE_NAME = `londyn-leaddev-v${CACHE_VERSION}`;
 const URLS_TO_CACHE = [
   './',
